46f255db1d79dd722723471fb7064ff2b40384d2
VishalDhiman010 <amansharma2313335@gmail.com>
2025-11-10 18:12:19 +0530

    Updated dynamic port for Render deployment

--- index.js
+++ index.js
@@ -47,5 +47,5 @@ app.use((req, res) => {
 
 });
 
-
-app.listen(5000, () => console.log("🚀 Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
